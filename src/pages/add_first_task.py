80160f92b87539a1cb67a844d9335ca19a3ee611
vitaliisotnichenko <vitalii.sotnichenko@gmail.com>
2020-01-21 00:12:57 +0200

    Add incapsulation

--- src/pages/add_first_task.py
+++ src/pages/add_first_task.py
@@ -4,5 +4,5 @@ from python_appium.src.pages.base_page import BasePage
 
 class AddFirstTask(BasePage):
     def click_at_add_first_task_button(self):
-        add_first_task_button = "Add First Task"
-        driver.find_element_by_accessibility_id(add_first_task_button).click()
+        __add_first_task_button = "Add First Task"
+        driver.find_element_by_accessibility_id(__add_first_task_button).click()
